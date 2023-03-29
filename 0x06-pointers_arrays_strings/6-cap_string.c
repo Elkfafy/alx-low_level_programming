cb0191921efcaf816a4f5b631f8fbaf0bac0e08f
elkfafy <hmdeslam.adel@gmail.com>
2023-03-29 17:47:40 +0200

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -28,7 +28,7 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 	{
 		if (contain("\t\n, ;.!?\"(){}", str[i]) &&
 			str[i + 1] >= 'a' &&
