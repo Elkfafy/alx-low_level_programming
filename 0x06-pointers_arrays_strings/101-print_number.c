706f8e695fd4ea8d51b55a3ee5d497e909fb9c4d
elkfafy <hmdeslam.adel@gmail.com>
2023-03-29 20:17:38 +0200

    9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - print number
+ * @n: input number
+ */
+void print_number(int n)
+{
+	int init;
+
+	init = 10000000;
+	if (n < 0)
+	{
+		init = -init;
+		_putchar('-');
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (n / init == 0)
+		init /= 10;
+	while (n != 0)
+	{
+		_putchar((n /init) + '0');
+		n %= init;
+		init /= 10;
+	}
+}
