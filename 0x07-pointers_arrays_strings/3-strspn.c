5e0e8817ebe0abf5193503a7283de19fe916f18d
elkfafy <hmdeslam.adel@gmail.com>
2023-04-03 17:35:11 +0200

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * isAccepted - is char in string
+ * @str: input string
+ * @c: input character
+ *
+ * Return: 1 if accepted 0 if not
+ */
+int isAccepted(char *str, char c)
+{
+	while (*str != '\0')
+	{
+		if (*str == c)
+			return (1);
+		str++;
+	}
+	return (0);
+}
+/**
+ * _strspn - get length of prefix substring
+ * @s: input string
+ * @accept: accepted characters
+ *
+ * Return: positive number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	for (i = 0; isAccepted(accept, s[i]); i++);
+	return (i);
+}
