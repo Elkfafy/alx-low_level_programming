e7376000fd804fbafd3b5b4fe3eb397976b4df71
elkfafy <hmdeslam.adel@gmail.com>
2023-04-03 17:21:12 +0200

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strchr - locate char in string
+ * @s: input string
+ * @c: character that needed to be located
+ *
+ * Return: pointer to character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != c; i++) { }
+	return (&s[i]);
+}
