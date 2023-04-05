1c9e8b0a2c3c29f1da56fe47724b7dc8cd9f6825
elkfafy <hmdeslam.adel@gmail.com>
2023-04-06 00:23:49 +0200

    5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * helper - help to get square
+ * @n: input number
+ * @i: input index
+ *
+ * Return: sqaure root
+ */
+int helper(int n, int i)
+{
+	if (n - i == 0)
+		return ((i / 2) + 1);
+	if (n - i < 0)
+		return (-1);
+	return (helper(n - i, i + 2));
+}
+/**
+ * _sqrt_recursion - get square root
+ * @n: input number
+ *
+ * Return: square root of the number
+ */
+int _sqrt_recursion(int n)
+{
+	return (helper(n, 1));
+}	
