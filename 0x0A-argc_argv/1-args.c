0a385477136d23b8c5b1dee02919d7dcc53902ad
elkfafy <hmdeslam.adel@gmail.com>
2023-04-07 20:10:44 +0200

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,10 +3,10 @@
  * main - print args
  * @args: number of args
  * @argv: the args
- * 
+ *
  * Return: 0
  */
-int main(int args, char *_[] __attribute__((unused)))
+int main(int args, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", args - 1);
 	return (0);
