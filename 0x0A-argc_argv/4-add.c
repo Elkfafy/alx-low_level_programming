649a7ca18467e4636701a930b6abf4b329118486
elkfafy <hmdeslam.adel@gmail.com>
2023-04-07 22:34:30 +0200

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add
+ * @args: number of args
+ * @argv: args
+ *
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+	int i, sum, temp;
+
+	if (args == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	sum = 0;
+	for (i = 0; i < args; i++)
+	{
+		if (argv[i] == '0')
+		{
+			continue;
+		}
+		temp = atoi(argv[i]);
+		if (temp == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += temp;
+	}
+	return (0);
+}
