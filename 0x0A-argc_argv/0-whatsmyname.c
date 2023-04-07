83ca18c17ad0801f34d85e04d07bea3d89959621
elkfafy <hmdeslam.adel@gmail.com>
2023-04-07 19:56:03 +0200

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print program name
+ * @args: number of args
+ * @argv: vector carry args
+ *
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+	puts(argv[args - args]);
+	return (0);
+}
