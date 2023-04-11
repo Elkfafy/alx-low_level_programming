01582a6847b1bb83ee0617f3e21a2a6b5c396deb
elkfafy <hmdeslam.adel@gmail.com>
2023-04-11 13:41:37 +0200

    5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * argstostr - conaticating args
+ * @ac: args count
+ * @av: args vector
+ *
+ * Return: concatinating string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int sumSize, i, j;
+
+	sumSize = 1;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			sumSize += 1;
+		}
+	}
+	str = (char *)malloc(sumSize * sizeof(char));
+	if (!sumSize)
+		return (NULL);
+	sumSize = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			str[sumSize] = av[i][j];
+			sumSize++;
+		}
+		str[sumSize] = '\n';
+		sumSize++;
+	}
+	str[sumSize] = '\0';
+	return (str);
+}
