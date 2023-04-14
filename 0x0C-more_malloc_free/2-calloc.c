06a1a3752348d7a1eabe0ee3ba9e0ee29f7ae38b
elkfafy <hmdeslam.adel@gmail.com>
2023-04-14 03:41:45 +0200

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _calloc : calloc function
+ * @nmemb: input number of members
+ * @size: size of each member
+ *
+ * Return: pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *arr;
+	char *temp;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (!arr)
+		return (NULL);
+	temp = (char *)arr;
+	for (i = 0; i < nmemb; i++)
+		*(temp + i) = '\0';
+	return (arr);
+}
