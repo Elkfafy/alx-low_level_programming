22120f0d2dccd4320260ad46a7a491234d86a355
elkfafy <hmdeslam.adel@gmail.com>
2023-04-14 03:20:56 +0200

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - malloc to b bytes
+ * @b: input bytes
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *arr;
+
+	arr = malloc(b);
+	if (!arr)
+		exit(98);
+	return (arr);
+}
