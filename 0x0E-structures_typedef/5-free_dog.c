cb10923afd50a165f5578f73de053f96bc1a9860
elkfafy <hmdeslam.adel@gmail.com>
2023-04-17 22:12:09 +0200

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * free_dog - free the dog
+ * @d: input dog
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
