828acb35229d4cceb8e7f4a2b4dc917a40ddfa06
elkfafy <hmdeslam.adel@gmail.com>
2023-04-20 01:07:21 +0200

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,6 +17,11 @@ int main(int args, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	op = get_op_func(argv[2]);
 	if (op == NULL)
 	{
