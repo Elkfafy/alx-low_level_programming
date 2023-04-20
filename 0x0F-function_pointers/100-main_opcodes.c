915c391068bc5b9bb98b11611f005268e5ae9053
elkfafy <hmdeslam.adel@gmail.com>
2023-04-20 02:44:50 +0200

    4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - get op code
+ * @args: arg size
+ * @argv: arguments vector
+ *
+ * Return: 0
+ */
+int main(int args, char *argv[])
+{
+	char *opc = (char *)main;
+	int i, nbytes;
+
+	if (args != 2)
+	{
+		printf("Error\n");
+		error(1);
+	}
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		error(2);
+	}
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i]);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
