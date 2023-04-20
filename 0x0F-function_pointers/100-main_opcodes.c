b9df79d1204248ae6b7338e537f46dd056e6a91b
elkfafy <hmdeslam.adel@gmail.com>
2023-04-20 02:49:08 +0200

    4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,17 +15,17 @@ int main(int args, char *argv[])
 	if (args != 2)
 	{
 		printf("Error\n");
-		error(1);
+		exit(1);
 	}
 	nbytes = atoi(argv[1]);
 	if (nbytes < 0)
 	{
 		printf("Error\n");
-		error(2);
+		exit(2);
 	}
 	for (i = 0; i < nbytes; i++)
 	{
-		printf("%02x", opc[i]);
+		printf("%02x", opc[i] & 0xff);
 		if (i != nbytes - 1)
 			printf(" ");
 	}
