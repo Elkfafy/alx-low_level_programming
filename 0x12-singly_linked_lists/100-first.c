b31fc69b191e7f1c801faf555df9ffc335c6bad3
elkfafy <hmdeslam.adel@gmail.com>
2023-04-27 13:45:35 +0200

    5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,9 @@
 /**
  * first - print first
  */
-void first() __attribute__((constructor));
-void first()
+void first(void) __attribute__((constructor));
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("%s%s", "You're beat! and yet, you must ",
+		       "allow,\nI bore my house upon my back!\n");
 }
