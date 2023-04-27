1a3a86551477a2223d91a25af05c0f3c3e407c8d
elkfafy <hmdeslam.adel@gmail.com>
2023-04-27 13:42:02 +0200

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,10 +8,11 @@
 size_t list_len(const list_t *h)
 {
 	unsigned int size = 1;
-	list_t *current = h->next;
+	list_t *current;
 
 	if (h == NULL)
 		return (0);
+	current = h->next;
 	while (current != NULL)
 	{
 		current = current->next;
