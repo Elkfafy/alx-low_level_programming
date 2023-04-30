ae6cb50123f3c174f3af5aaa1022fcc1fc678d09
elkfafy <hmdeslam.adel@gmail.com>
2023-04-30 06:48:12 +0300

    6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -10,7 +10,7 @@ int pop_listint(listint_t **head)
 	listint_t *current = *head;
 	int value = (*head)->n;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
 	*head = (*head)->next;
 	free(current);
