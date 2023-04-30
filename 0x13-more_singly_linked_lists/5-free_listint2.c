7c28ac488d8cd6963bf8a9c58c4d5cf4a3dea6db
elkfafy <hmdeslam.adel@gmail.com>
2023-04-30 05:37:43 +0200

    5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,11 +5,11 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (*head == NULL || head == NULL)
+	if (*head == NULL)
 		return;
 	free_listint2(&((*head)->next));
 	(*head)->next = NULL;
 	free(*head);
+	free(head);
 	*head = NULL;
-	head = NULL;
 }
