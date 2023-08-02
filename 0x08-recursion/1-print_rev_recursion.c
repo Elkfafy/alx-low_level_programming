f61bc9a39fbaa13c537a2b4ae3382f76a0f7a685
elkfafy <hmdeslam.adel@gmail.com>
2023-08-02 17:45:51 +0300

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * rev_recursion - print reverse
+ * @s: input string
+ */
+void rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	rev_recursion(s + 1);
+	_putchar(*s);
+}
+/**
+ * _print_rev_recursion - print reverse with new line
+ * @s: input string
+ */
+void _print_rev_recursion(char *s)
+{
+	rev_recursion(s);
+	_putchar('\n');
+}
