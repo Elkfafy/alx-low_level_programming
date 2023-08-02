7076b501730a01fc23a29072fb4f13f4c5db9f00
elkfafy <hmdeslam.adel@gmail.com>
2023-08-03 00:35:10 +0300

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,31 +1,5 @@
 #include "main.h"
 /**
- * wild_helper - help to wild
- * @s1: first string
- * @s2: second string
- * @is_wild: if wild before
- *
- * Return: true or false
- */
-int wild_helper(char *s1, char *s2, int is_wild)
-{
-	if (*s2 == '*')
-		return (wild_helper(s1, s2 + 1, 1));
-	if (*s1 == '\0' && *s2 != '\0')
-		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s2 == '\0' && is_wild == 0)
-		return (0);
-	if (*s2 == '\0' && is_wild == 1)
-		return (1);
-	if (*s1 != *s2 && is_wild == 0)
-		return (0);
-	if (*s1 != *s2 && is_wild == 1)
-		return (wild_helper(s1 + 1, s2, 1));
-	return (wild_helper(s1 + 1, s2 + 1, 0));
-}
-/**
  * wildcmp - wild compare
  * @s1: input first string
  * @s2: input wild second string
@@ -34,5 +8,16 @@ int wild_helper(char *s1, char *s2, int is_wild)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (wild_helper(s1, s2, 0));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
 }
