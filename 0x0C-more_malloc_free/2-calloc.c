ffb8c4d78ea63d39fe03a88bcd3859ee57c92862
elkfafy <hmdeslam.adel@gmail.com>
2023-08-10 16:18:59 +0300

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+ * _memset - set memory to a value
+ * @ptr: input pointer
+ * @val: input value
+ * @size: size of the array
+ *
+ * Return: a pointer
+ */
+void *_memset(void *ptr, int val, size_t size)
+{
+	char *temp = ptr;
+
+	while (size-- > 0)
+		*temp++ = val;
+	return (ptr);
+}
+/**
  * _calloc - calculate malloc
  * @nmemb: input array of nmemb
  * @size: number of array elements
@@ -8,5 +24,11 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	return (malloc(nmemb * size));
+	void *result;
+
+	result = malloc(nmemb * size);
+	if (result == NULL)
+		return (NULL);
+	result = _memset(result, 0, nmemb * size);
+	return (result);
 }
