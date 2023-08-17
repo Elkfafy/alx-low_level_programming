43df12ba0192cb9dbbc0782798242dfb97bf5bb4
elkfafy <hmdeslam.adel@gmail.com>
2023-08-17 16:54:55 +0300

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,64 @@
+#include "variadic_functions.h"
+/**
+ * print_form - print depending on the format
+ * @c: input format character
+ * @args: input va list
+ */
+void print_form(char c, va_list *args)
+{
+	char *cont;
+
+	switch (c)
+	{
+		case 'c':
+			printf("%c", va_arg(*args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(*args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(*args, double));
+			break;
+		case 's':
+			cont = va_arg(*args, char *);
+			if (cont == NULL)
+				cont = "(nil)";
+			printf("%s", cont);
+			break;
+	}
+}
+/**
+ * print_all - print all depending on format string
+ * format: input format string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *seperator = ", ";
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	va_start(args, format);
+	print_form(format[i], &args);
+	i++;
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+			case 'i':
+			case 'f':
+			case 's':
+				printf("%s", seperator);
+				break;
+		}
+		print_form(format[i], &args);
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
