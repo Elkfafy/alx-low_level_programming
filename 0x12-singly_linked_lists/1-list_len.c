53c3297abf1602fe65b2b2b6a631695cff629e4a
elkfafy <hmdeslam.adel@gmail.com>
2023-08-24 16:07:52 +0300

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - get length of linked list
+ * @h: input head of list
+ *
+ * Return: length of the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
