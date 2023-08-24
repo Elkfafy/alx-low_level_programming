de9ca8426f0baa1bf4c880a8da4c0986771ff4c2
elkfafy <hmdeslam.adel@gmail.com>
2023-08-24 17:04:23 +0300

    2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,5 @@
 #include "lists.h"
 /**
- * str_len - get length of string
- * @str: input string
- *
- * Return: string length
- */
-size_t str_len(char *str)
-{
-	int size;
-
-	for (size = 0; str[size] != '\0'; size++)
-		;
-	return (size);
-}
-/**
  * print_list - print elements of linked list
  * @h: input linked list head
  *
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include "string.h"
+/**
+ * str_len - get length of str
+ * @str: input string
+ *
+ * Return: return string length
+ */
+unsigned int str_len(char *str)
+{
+	unsigned int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	       ;
+	return (len);
+}	
+/**
+ * add_node - add a node
+ * @head: input head
+ * @str: input node string
+ *
+ * Return: head of list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (str && !(new_node->str))
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = str_len(new_node->str);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
