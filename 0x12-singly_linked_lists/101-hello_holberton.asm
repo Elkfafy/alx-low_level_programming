274d7a3a76d548348ae2f39f57548e15d3c5bd51
elkfafy <hmdeslam.adel@gmail.com>
2023-08-25 01:10:54 +0300

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,19 @@
-mov rdi, formtStr
-mov rsi, Str
-mov al, 0
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-extern printf
-call printf
+    section .text
+    global main
+    extern printf
 
-ret
+main:
+    push rbp
+    mov rbp, rsp
 
-formtatStr:
-	db `%s\n`,0
-Str:
-	db "Hello, Holberton",10,0
+    mov rsi, hello
+    mov rdi, format
+    call printf
+
+    pop rbp
+    mov rax, 0
+    ret
