5006f69776ee75903d4dedaf3c6cda8a2611852e
elkfafy <hmdeslam.adel@gmail.com>
2023-11-27 18:28:30 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = creat(filename, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT| O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	for (text_size = 0; text_content[text_size] != '\0'; text_size++)
