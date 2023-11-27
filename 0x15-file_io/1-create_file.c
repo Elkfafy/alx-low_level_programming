4fb72a123f838b849966dfa6bcea1e7f863ed0a3
elkfafy <hmdeslam.adel@gmail.com>
2023-11-27 17:15:49 +0200

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * create_file - create file
+ * @filename: file name
+ * @text_content: text created
+ * Return: success or not
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, res, text_size;
+
+	if (filename == NULL)
+		return (-1);
+	fd = creat(filename, O_RDWR);
+	if (fd < 0)
+		return (-1);
+	for (text_size = 0; text_content[text_size] != '\0'; text_size++)
+		;
+	res = write(fd, text_content, text_size);
+	if (res < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
