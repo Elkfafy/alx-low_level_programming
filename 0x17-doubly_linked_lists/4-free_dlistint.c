7ca3a4482e2dd24141c83894d54d251c61f43acd
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-14 10:46:28 +0200

    4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - free list
+ * @head: input head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
