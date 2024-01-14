ddeb6b43c2ead9e5f94d9496b7c7d0c349b003ea
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-14 10:30:22 +0200

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add node to list
+ * @head: input head
+ * @n: input number
+ * Return: address of new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+	return (new);
+}
