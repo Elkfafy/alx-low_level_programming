e0d099c31d97ad5c32e10e17f4c05b42176995fb
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-14 13:17:44 +0200

    7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert at index
+ * @h: input head
+ * @idx: index
+ * @n: input number
+ * Return: new address
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *new, *current;
+
+	if (h == NULL)
+		return (NULL);
+	current = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	while (current->next != NULL && count < idx - 1)
+	{
+		current = current->next;
+		count++;
+	}
+	if (current->next == NULL && count != idx - 1)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = current->next;
+	new->prev = current;
+	current->next = new;
+	return (current);
+}
