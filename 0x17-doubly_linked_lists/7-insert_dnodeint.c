eaaed052784abbebfd7a25d9fadcfb3b64c00ec9
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-01-14 13:20:12 +0200

    7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	if (idx == 0)
 		return (add_dnodeint(h, n));
+	if (current == NULL)
+		return (NULL);
 	while (current->next != NULL && count < idx - 1)
 	{
 		current = current->next;
