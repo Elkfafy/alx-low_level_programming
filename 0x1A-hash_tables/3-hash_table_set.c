e6beb52ed6731bf88fa87acd5cbddedf71b35bea
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-02 21:03:52 +0200

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,6 +23,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node, *current;
 
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
+		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
