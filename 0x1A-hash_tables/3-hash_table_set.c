92d9a14b6ef84dd376bb66f1eb3e67f26687ff44
ahmed elkfafy <hmdeslam.adel@gmail.com>
2024-02-02 21:35:02 +0200

    3-3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,7 +21,7 @@ void free_node(hash_node_t *node)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current;
 
 	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (0);
@@ -36,6 +36,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;
 	else
 	{
+		current = ht->array[index];
+		if (strcmp(current->key, key) == 0)
+		{
+			new_node->next = current->next;
+			ht->array[index] = new_node;
+			free_node(current);
+			return (1);
+		}
+		while (current->next != NULL && strcmp(current->next->key, key) != 0)
+		{
+			current = current->next;
+		}
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
